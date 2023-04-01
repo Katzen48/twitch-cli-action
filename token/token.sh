de60977b7ee266eaf4130acb09c188d6e8975547
Katzen48 | Tobi <Katzen48@freenet.de>
2023-04-01 23:20:28 +0200

    fixed token

--- token/token.sh
+++ token/token.sh
@@ -3,6 +3,6 @@ if [ -n "$ACTION_TWITCH_CLIENT_ID" ] && [ -n "$ACTION_TWITCH_TOKEN" ]
 then
   eval "twitch token -r $ACTION_TWITCH_TOKEN --client-id $ACTION_TWITCH_CLIENT_ID"
 else
-  TOKEN=$(eval "twitch token" | awk '{print $5}')
-  echo "token=$($TOKEN)" >> $GITHUB_OUTPUT
+  TOKEN=$(twitch token | awk '{print $5}')
+  echo "token=$($TOKEN)" >> "$GITHUB_OUTPUT"
 fi
