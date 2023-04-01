b323b7c727abee581c2bfa536a38cffa8d23ab46
Katzen48 | Tobi <Katzen48@freenet.de>
2023-04-02 00:31:52 +0200

    fixed api call

--- api/api.sh
+++ api/api.sh
@@ -3,7 +3,7 @@ COMMAND="twitch api $ACTION_TWITCH_METHOD $ACTION_TWITCH_ENDPOINT"
 
 if [ -n "$ACTION_TWITCH_QUERY_PARAMETERS" ]
 then
-  for parameter in ${ACTION_TWITCH_EVENTS//,/ }
+  for parameter in ${ACTION_TWITCH_QUERY_PARAMETERS//,/ }
   do
     COMMAND="$COMMAND -q $parameter"
   done
